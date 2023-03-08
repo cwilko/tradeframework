5b620cc4d50ff91c26c7479ee17615a6b6f7475c
cwilko <chris.wilkinson@uk.ibm.com>
2023-03-08 23:03:33 +0000

    Migrate to TradeFramework 0.2.0

---
+++ tradeframework/operations/utils.py
@@ -0,0 +1,12 @@
+from tradeframework.api.core import Asset
+import pandas as pd
+
+# Helper method to merge bar and gap returns into a single period
+
+
+def getPeriodReturns(returns):
+    return pd.DataFrame((returns + 1).prod(axis=1) - 1, columns=["period"])
+
+
+def createAssetFromOHLC(index, ohlc, name="OHLCData"):
+    return Asset(name, pd.DataFrame(ohlc, index=index, columns=["Open", "High", "Low", "Close"]))
