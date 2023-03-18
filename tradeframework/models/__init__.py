0304854e945255ad1a2e7e14be205c57e8814527
cwilko <chris.wilkinson@uk.ibm.com>
2023-03-18 20:33:01 +0000

    Move REST API clients to marketinsights-remote

--- tradeframework/models/__init__.py
+++ tradeframework/models/__init__.py
@@ -1,7 +1,5 @@
 from .preOpenMomentum import PreOpenMomentum
 from .buyAndHold import BuyAndHold
-from .miBasic import MIBasicModel
-from .miAgg import MIAggregateModel
 from .sellAndHold import SellAndHold
 from .meanReversion import MeanReversion
 from .trendFollowing import TrendFollowing
